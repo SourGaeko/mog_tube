88d79b15c16543bee18e283880961919a9360887
SourGaeko <66372255+SourGaeko@users.noreply.github.com>
2020-09-16 00:49:36 +0900

    20200916 MOG_TUBE

---
+++ controllers/videoController.js
@@ -0,0 +1,40 @@
+import routes from "../routes";
+import Video from "../models/Video";
+
+export const home = async(req, res) => {
+    try{
+        const videos = await Video.find({});
+        res.render("home", { pageTitle : "Home", videos });
+    } catch (error) {
+        console.log(error);
+        res.render("home", { pageTitle : "Home", videos: [] });  
+    }
+};
+
+export const searchVideos = (req, res) => {
+    const {
+        query: { term: searchingBy }
+        } = req
+    // === const searchingBy = req.query.term;
+    res.render("search", { pageTitle : "Search", searchingBy, videos });
+}
+export const getUpload = (req, res) => 
+    res.render("upload", { pageTitle : "Upload" });
+
+export const postUpload = async(req, res) => {
+    const { 
+        body: { title, description },
+        file : { path }
+     } = req;
+     const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description
+     })
+     console.log(newVideo)
+     res.redirect(routes.videoDetail(newVideo.id));
+};
+
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle : "Video Details" });
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle : "Edit Video" });
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle : "Delete Video" });
